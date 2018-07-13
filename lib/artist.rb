a208aad1be8542ab8b75fcb4a3555cbf6c10ea75
Priscilla H <priscillamhuynh@gmail.com>
2018-07-13 10:11:23 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,20 @@
+class Artist 
+  attr_accessor :name, :songs 
+  @@all=[] 
+
+  def self.all 
+    @@all 
+  end 
+
+  def initialize 
+    save 
+  end 
+    
+  def save 
+    @@all<self 
+  end   
+
+  def self.reset_all
+    @@all.clear 
+  end 
+end 
